f1599bda2153a8cb6c5fc43232acbbb43793eed1
Andrew <andrewpvalley@gmail.com>
2022-08-21 15:06:22 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,22 @@ import HelloWorldScene from './scenes/HelloWorldScene'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	scale: {
+		width: 800,
+		height: 600
+	},
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 }
+			gravity: { y: 0 }
 		}
 	},
-	scene: [HelloWorldScene]
+	dom: {
+        createContainer: true
+    },
+	scene: [HelloWorldScene],
+	pixelArt: true,
+
 }
 
 export default new Phaser.Game(config)
--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -1,4 +1,8 @@
 import Phaser from 'phaser'
+import Player from '../Actors/Player'; 
+import { listenTest } from '../Api/Server';
+
+
 
 export default class HelloWorldScene extends Phaser.Scene
 {
@@ -9,31 +13,16 @@ export default class HelloWorldScene extends Phaser.Scene
 
 	preload()
     {
-        this.load.setBaseURL('http://labs.phaser.io')
-
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
+        listenTest();
+        this.load.image('test', 'test.png')
+        this.scene.add('Player', Player, true);
     }
 
     create()
     {
-        this.add.image(400, 300, 'sky')
 
-        const particles = this.add.particles('red')
+        this.add.image(400, 300, 'test')
+        this.scene.launch('Player')
 
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
-
-        const logo = this.physics.add.image(400, 100, 'logo')
-
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
-
-        emitter.startFollow(logo)
     }
 }
